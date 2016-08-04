69ab151e194d3ada8e817cf01516a0f861c99ff9
Jonathan Stowe <jns@gellyfish.co.uk>
2016-08-04 21:22:27 +0100

    Up version

--- lib/Test/META.pm
+++ lib/Test/META.pm
@@ -60,7 +60,7 @@ though they won't typically be needed.
 =end pod
 
 
-module Test::META:ver<0.0.6>:auth<github:jonathanstowe> {
+module Test::META:ver<0.0.7>:auth<github:jonathanstowe> {
 
     use Test;
     use META6:ver(v0.0.4..*);
